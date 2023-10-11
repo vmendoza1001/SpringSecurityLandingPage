15bce0ab72d48ee1c6aec347dde905153f635e4e
Vanessa Mendoza <v.mendoza1@gmail.com>
2023-10-11 00:16:16 -0400

    Created DemoSecurityConfig class

---
+++ SpringSecurityWithLandingPage/src/main/java/com/example/SpringSecurityWithLandingPage/SpringSecurityWithLandingPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurityWithLandingPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithLandingPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithLandingPageApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityWithLandingPage/src/main/java/com/example/SpringSecurityWithLandingPage/controller/DemoController.java
@@ -0,0 +1,4 @@
+package com.example.SpringSecurityWithLandingPage.controller;
+
+public class DemoController {
+}
